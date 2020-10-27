aed4b53401e3750b9ad96143cc2eb19b61053b47
jhavl <jhavl@users.noreply.github.com>
2020-10-27 17:32:05 +1000

    progress

--- public/js/index.js
+++ public/js/index.js
@@ -25,6 +25,18 @@ let first_step = 0;
 // // scene recorder
 // let rec = null
 
+// var ws = new WebSocket("ws://127.0.0.1:5678/"),
+// messages = document.createElement('ul');
+// ws.onmessage = function (event) {
+// 	var messages = document.getElementsByTagName('ul')[0],
+// 	message = document.createElement('li'),
+// 	content = document.createTextNode(event.data);
+// 	console.log(event.data)
+// message.appendChild(content);
+// messages.appendChild(message);
+// };
+// document.body.appendChild(messages);
+
 setInterval(rt_heartbeat, 10)
 init()
 animate();
