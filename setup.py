a39eacde60db1183b8f83a9a0b4dff5c0798981f
jhavl <jhavl@users.noreply.github.com>
2020-11-09 14:16:24 +1000

    first release

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ extra_files = package_files('swift/public')
 setup(
     name='swift',
 
-    version='0.1.0',
+    version='0.6.0',
 
     description='A Python/Javascript Visualiser',
 
@@ -44,9 +44,30 @@ setup(
 
     license='MIT',
 
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
     python_requires='>=3.6',
 
-    keywords='robotics vision arm kinematics ros',
+    keywords='python robotics robotics-toolbox kinematics dynamics' \
+             ' motion-planning trajectory-generation jacobian hessian' \
+             ' control simulation robot-manipulator mobile-robot',
 
     packages=find_packages(exclude=["tests", "examples"]),
 
