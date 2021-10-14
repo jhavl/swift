6c7bad86edd61e11230f20e82418f423b5ae9d2d
jhavl <jhavl@users.noreply.github.com>
2021-10-14 21:27:41 +1000

    basic implementation of camera

--- swift/SwiftCamera.py
+++ swift/SwiftCamera.py
@@ -4,10 +4,12 @@
 """
 
 from spatialmath import SE3
+import spatialgeometry as sg
 import numpy as np
+import uuid
 
 
-class SwiftCamera:
+class Camera(sg.Shape):
     def __init__(
         self,
         width=None,
@@ -17,10 +19,28 @@ class SwiftCamera:
         fx=None,
         cy=None,
         cx=None,
-        base=None,
-        shape=None,
+        **kwargs
     ):
+        super().__init__(stype="camera", **kwargs)
 
         self.width = width
         self.height = height
         self.fov = fov
+
+        self._id = str(uuid.uuid4())
+        print(self._id)
+
+    def to_dict(self):
+        """
+        to_dict() returns the shapes information in dictionary form
+
+        :returns: All information about the shape
+        :rtype: dict
+        """
+
+        shape = super().to_dict()
+        shape["width"] = self.width
+        shape["height"] = self.height
+        shape["fov"] = self.fov
+        shape["id"] = self._id
+        return shape
