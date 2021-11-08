43248cc1083b29ed2a2f9467b557a70d15613fb7
jhavl <jhavl@users.noreply.github.com>
2021-11-08 10:03:00 +1000

    formatted

--- next-swift/server.ts
+++ next-swift/server.ts
@@ -1,29 +1,30 @@
 const express = require('express')
 const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ 'dev': dev })
+const app = next({ dev: dev })
 const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  const server = express()
+app.prepare()
+    .then(() => {
+        const server = express()
 
-  server.get('*', (req, res) => {
+        server.get('*', (req, res) => {
+            if (req.path.startsWith('/retrieve/')) {
+                const path = req.path.slice(10)
+                res.sendFile(path)
+            } else {
+                return handle(req, res)
+            }
+        })
 
-    if (req.path.startsWith('/retrieve/')) {
-      const path = req.path.slice(10);
-      res.sendFile(path);
-    } else {
-      return handle(req, res)
-    }
-  })
+        server.listen(3000, (err) => {
+            if (err) throw err
+            console.log('> Ready on http://localhost:3000')
+        })
+    })
+    .catch((ex) => {
+        console.error(ex.stack)
+        process.exit(1)
+    })
 
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-}).catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
-
-export { }
+export {}
