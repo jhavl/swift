71324d096f44ad2f0be610dc5e9744a4c8ee6f36
jhavl <jhavl@users.noreply.github.com>
2022-05-12 14:30:36 +1000

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import numpy
 
 here = path.abspath(path.dirname(__file__))
 
-req = ["numpy>=1.17.4", "spatialgeometry>=0.2.0", "websockets"]
+req = ["numpy>=1.17.4", "spatialgeometry~=1.0.0", "websockets"]
 
 # Get the long description from the README file
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
@@ -42,7 +42,7 @@ phys = Extension(
 
 setup(
     name="swift-sim",
-    version="0.10.0",
+    version="1.0.0",
     description="A Python/Javascript Visualiser",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -53,7 +53,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         # Indicate who your project is intended for
         "Intended Audience :: Developers",
         # Pick your license as you wish (should match "license" above)
@@ -64,6 +64,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     python_requires=">=3.6",
     keywords="python robotics robotics-toolbox kinematics dynamics"
